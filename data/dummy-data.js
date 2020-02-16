ac24fd628f088255c0ec5e16ede5c07ee0275339
nhatmoc97 <nhatmoc97@gmail.com>
2020-02-16 21:52:30 +0700

    Add Shopping folder

---
+++ data/dummy-data.js
@@ -0,0 +1,54 @@
+import Product from '../models/product';
+
+const PRODUCTS = [
+    new Product(
+        'p1',
+        'u1',
+        'Durex Extra Thin',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/Durex-Inv-Sen-3s-FRONT_1024x1024@2x.png?v=1564748981',
+        'Thiết kế dày hơn với lượng gel bôi trơn nhiều hơn, tăng cảm giác an toàn mà không mất đi sự gần gũi cần thiết.',
+        30.0000
+    ),
+    new Product(
+        'p2',
+        'u1',
+        'Durex Kingtex',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/VN_Kingtex_pk3_1024x1024@2x.jpg?v=1563973001',
+        'Bao cao su Durex Kingtex đã được thiết kế để cung cấp cho bạn một sự vừa vặn hơn bao giờ hết.',
+        50.0000
+    ),
+    new Product(
+        'p3',
+        'u2',
+        'Durex Fetherlite',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/Durex_Fetherlite_3_Front_1024x1024@2x.png?v=1563973826',
+        'Thiết kế dày hơn với lượng gel bôi trơn nhiều hơn, tăng cảm giác an toàn mà không mất đi sự gần gũi cần thiết',
+        100.000
+    ),
+    new Product(
+        'p4',
+        'u3',
+        'Durex Performa',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/Performa_3_1024x1024@2x.png?v=1563970989',
+        "Không chỉ mang đến trải nghiệm đáng nhớ, Durex Perfoma còn có mùi hương dễ chịu, khiến bạn hoàn toàn thư giãn và thoải mái tận hưởng cuộc yêu.",
+        120.000
+    ),
+    new Product(
+        'p5',
+        'u3',
+        'Durex Pleasure Max',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/Durex_Pleasuremax_3_Front_1024x1024@2x.png?v=1563972051',
+        'Durex PleasureMax với kết cấu gân và hạt giúp tăng cường khoái cảm cho cuộc yêu, thêm kích thích mãnh liệt cho lứa đôi.',
+        100.000
+    ),
+    new Product(
+        'p6',
+        'u1',
+        'Durex Love',
+        'https://cdn.shopify.com/s/files/1/0104/6662/3554/products/Durex_Love_3_Front_1024x1024@2x.png?v=1564485460',
+        "Thiết kế dày hơn với lượng gel bôi trơn nhiều hơn, tăng cảm giác an toàn mà không mất đi sự gần gũi cần thiết.",
+        120.000
+    )
+];
+
+export default PRODUCTS;
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import Colors from '../constants/Colors';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
+import { Ionicons } from '@expo/vector-icons';
+const defaultNavigationOptions = {
+
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+
+};
+const ProductsNavigator = createStackNavigator(
+    {
+        ProductsOverview: ProductsOverviewScreen,
+        ProductDetail: ProductDetailScreen,
+        Cart: CartScreen
+    },
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (<Ionicons name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                size={23}
+                color={drawerConfig.tintColor}
+            />)
+        },
+        defaultNavigationOptions: defaultNavigationOptions
+    }
+);
+const OrdersNavigator = createStackNavigator({
+    Orders: OrdersScreen
+},
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (<Ionicons name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+                size={23}
+                color={drawerConfig.tintColor}
+            />)
+        },
+        defaultNavigationOptions: defaultNavigationOptions
+    }
+);
+const ShopNavigator = createDrawerNavigator({
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator
+},
+    {
+        contentOptions: {
+            activeTintColor: Colors.primary
+        }
+    });
+export default createAppContainer(ShopNavigator);
